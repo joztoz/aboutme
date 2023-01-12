8f65183dbda0b0cab3eaa8afa8349ee0a86b77e5
Jozef Ziak <jozef_ziak@datalan.sk>
2023-01-12 09:31:48 +0100

    no images

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,3 @@
-<script>
-    import reminders from "./src/assets/reminders.png"; 
-    import crave from "./src/assets/crave.png"; 
-    import trulo from "./src/assets/trulo.png"; 
-    import dudes from "./src/assets/dudes.png"; 
-    import tunes from "./src/assets/tunes.png"; 
-    import hamingway from "./src/assets/hamingway.gif"; 
-</script>
 
 
 <div class="flex flex-wrap gap-4 justify-center">
