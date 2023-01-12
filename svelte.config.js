a9ebb9cc2dda97a7f776580f62a7ac36fbca9a6e
Jozef Ziak <jozef_ziak@datalan.sk>
2023-01-12 08:52:17 +0100

    second commit

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,25 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	kit: {
-	  adapter: adapter()
+		adapter: adapter()
 	},
+	
 	preprocess: vitePreprocess()
-  };
-  
-  export default config;
+};
+
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: '200.html',
+      precompress: false,
+      strict: true
+    })
+  }
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  darkMode: false,
   content: [
     "./src/**/*.{html,js,svelte,ts}",
     "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
